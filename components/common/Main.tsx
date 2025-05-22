38318d2584865183553d533beb37a1c23491eb3f
tidypr <tidypr@icloud.com>
2025-05-22 14:23:50 +0900

    mod: components

--- components/common/Main.tsx
+++ components/common/Main.tsx
@@ -1,3 +1,3 @@
 export default function Main({ children }: { children: React.ReactNode }) {
-  return <main className='container'>{children}</main>;
+  return <main>{children}</main>;
 }
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,12 +7,14 @@ import UserMenu from './UserMenu';
 export default function Header() {
   return (
     <header className='fixed inset-x-0 top-0 z-50 flex h-16 items-center justify-between bg-white/80 px-4 py-2 backdrop-blur-md dark:bg-neutral-900/80'>
-      <Logo />
-      <InputSearch />
-      <Navbar />
-      <div className='flex items-center gap-2'>
-        <ModeToggle />
-        <UserMenu />
+      <div className='mx-auto max-w-7xl px-4 w-full flex items-center justify-between'>
+        <Logo />
+        <InputSearch />
+        <Navbar />
+        <div className='flex items-center gap-2'>
+          <ModeToggle />
+          <UserMenu />
+        </div>
       </div>
     </header>
   );
