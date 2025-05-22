248c6f0de1ac5d7c461a6ff62747867407456747
tidypr <tidypr@icloud.com>
2025-05-22 14:22:45 +0900

    mod: pages

--- app/(pages)/portfolio/page.tsx
+++ app/(pages)/portfolio/page.tsx
@@ -1,7 +1,30 @@
+import Contact from '@/components/portfolio/contact/Contact';
+import Intro from '@/components/portfolio/intro/Intro';
+import PortfolioSection from '@/components/portfolio/PortfolioSection';
+import ProjectList from '@/components/portfolio/projects/ProjectList';
+import Skiils from '@/components/portfolio/Skiils';
+
 export default function PortfolioPage() {
+  // const dummyData = Array.from({ length: 10 }, (_, index) => ({
+  //   id: Math.random() * 1000,
+  //   title: `프로젝트 ${index + 1}`,
+  //   context: `This is the context for card ${index + 1}. Here you can put any relevant information you want to display.`,
+  // }));
+
   return (
-    <>
-      <h1>page</h1>
-    </>
+    <div className='flex w-full flex-col gap-8 px-2 pt-24 md:px-4'>
+      <PortfolioSection id='intro'>
+        <Intro />
+      </PortfolioSection>
+      <PortfolioSection id='skills' title='skills'>
+        <Skiils />
+      </PortfolioSection>
+      <PortfolioSection id='projects' title='projects'>
+        <ProjectList />
+      </PortfolioSection>
+      <PortfolioSection id='contact' title='contact'>
+        <Contact />
+      </PortfolioSection>
+    </div>
   );
 }
---
+++ app/(pages)/project/[id]/page.tsx
@@ -0,0 +1,21 @@
+export default function ProjectDetailPage() {
+  return (
+    <div className='flex h-full w-full flex-col items-center justify-center pt-24'>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+      <h1>ProjectDetailPage</h1>
+    </div>
+  );
+}
